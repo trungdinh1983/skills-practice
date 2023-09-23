2db67dda19e4df6bbfa72c2748feac510ea7f887
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-09-22 19:52:42 -0700

    Alignment

--- practice-javascript.js
+++ practice-javascript.js
@@ -7,6 +7,8 @@
 
 //  # Problem -----------------------------------------------------------------
 
+Basic Hash: Menu Pair
+
  Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
 
  Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
