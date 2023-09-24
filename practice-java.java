b69f45d799581ba0f780513c32fccd2b916d1337
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-09-24 10:43:04 -0700

    value pair

--- practice-java.java
+++ practice-java.java
@@ -9,8 +9,38 @@
 //# Problem -----------------------------------------------------------------
 
 
+Basic Hash: Menu Pair
+
+ Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+ Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+ 
+ Output: 14
+ 
+ Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+ 
+ 
 
 // # Solution =============================================
+
+# Initialize variables
+menu = {
+    "hot dog": 2,
+    "hamburger": 3,
+    "steak sandwich": 5,
+    "fries": 1,
+    "cole slaw": 1,
+    "soda": 2
+}
+total_price = 0
+
+# We need keys from the dictionary to loop through it
+for food_item in menu.keys():
+    # Add its price to total_price
+    total_price += menu[food_item]  # total_price = total_price + menu[food_item]
+
+# Print the total_price
+print(f"Total price of one of everything: {total_price}")  # Output should be 14
 
 
 
