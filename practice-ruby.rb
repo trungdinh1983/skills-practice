7c0c77d237634164e717d90dbf0de0ee54e9e41c
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-10-13 08:47:26 -0700

    ETL #2

--- practice-ruby.rb
+++ practice-ruby.rb
@@ -1,10 +1,130 @@
 
-    # Problem -----------------------------------------------------------------
+        # Problem -----------------------------------------------------------------
 
 
 
 # Solution =============================================
 
+    
+    
+    # Problem -----------------------------------------------------------------
+
+Data transformation: ETL #2
+
+    You are given a hash in format #A, and you are to return the same data as a hash using format #B, as specified below:
+
+    Input:
+    
+    {
+    1 => ["A", "E", "I", "O", "U"]
+    }
+    
+    Output:
+    
+    {
+    'a' => 1,
+    'e' => 1,
+    'i' => 1,
+    'o' => 1,
+    'u' => 1
+    }
+    Input:
+    
+    {
+    1 => ["A", "E"],
+    2 => ["D", "G"]
+    }
+    
+    Output:
+    
+    {
+    'a' => 1,
+    'd' => 2,
+    'e' => 1,
+    'g' => 2
+    }
+    
+    Input:
+    
+    {
+    1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2 => ["D", "G"],
+    3 => ["B", "C", "M", "P"],
+    4 => ["F", "H", "V", "W", "Y"],
+    5 => ["K"],
+    8 => ["J", "X"],
+    10 => ["Q", "Z"]
+    }
+    
+    Output:
+    
+    {
+    'a' => 1,
+    'b' => 3,
+    'c' => 3,
+    'd' => 2,
+    'e' => 1,
+    'f' => 4,
+    'g' => 2,
+    'h' => 4,
+    'i' => 1,
+    'j' => 8,
+    'k' => 5,
+    'l' => 1,
+    'm' => 3,
+    'n' => 1,
+    'o' => 1,
+    'p' => 3,
+    'q' => 10,
+    'r' => 1,
+    's' => 1,
+    't' => 1,
+    'u' => 1,
+    'v' => 4,
+    'w' => 4,
+    'x' => 8,
+    'y' => 4,
+    'z' => 10
+    }
+
+# Solution =============================================
+
+# Initialize variables
+input_hash = {
+  1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2 => ["D", "G"],
+  3 => ["B", "C", "M", "P"],
+  4 => ["F", "H", "V", "W", "Y"],
+  5 => ["K"],
+  8 => ["J", "X"],
+  10 => ["Q", "Z"]
+}
+
+output_hash = {}
+
+# Loop through the input hash
+i = 0
+keys = input_hash.keys
+while i < keys.length
+  key = keys[i]
+  value_array = input_hash[key]
+  
+  j = 0
+  while j < value_array.length
+    # Convert each letter to lowercase and add to output hash
+    letter = value_array[j].downcase
+    output_hash[letter] = key
+    
+    # Move to the next letter in the value array
+    j += 1
+  end
+  
+  # Move to the next key in the input hash
+  i += 1
+end
+
+# Print the result
+puts "Transformed hash is: #{output_hash}"
 
   
   # Problem -----------------------------------------------------------------
