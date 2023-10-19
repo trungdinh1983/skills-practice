9cd8a1e7aff2209d1b3c8797e515d7c80b34a077
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-10-19 10:09:17 -0700

    Book Organizer Ruby

--- practice-ruby.rb
+++ practice-ruby.rb
@@ -7,6 +7,79 @@
 
     
 
+
+        # Problem -----------------------------------------------------------------
+
+        Data Transformation: Book Organizer
+
+        Given a list of books provided in this format:
+        
+        [
+        {title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
+        {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+        {title: "1984", author: "George Orwell", year: 1949 },
+        {title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
+        {title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
+        {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+        {title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
+        ]
+        
+        return the data in this new author-centric format:
+        
+        { "J. R. R. Tolkien" => [
+        {title: "The Lord of the Rings", year: 1954 },
+        {title: "The Hobbit", year: 1937 },
+        {title: "The Two Towers", year: 1954 }
+        ],
+        "Harper Lee" => [
+        {title: "To Kill a Mockingbird", year: 1960 },
+        {title: "Go Set a Watchman", year: 2015 }
+        ],
+        "George Orwell" => [
+        {title: "1984", year: 1949 }
+        ],
+        "F. Scott Fitzgerald" => [
+        {title: "The Great Gatsby", year: 1925 }
+        ]
+        }
+
+
+# Solution =============================================
+
+    # Given list of books
+books = [
+  {title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
+  {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  {title: "1984", author: "George Orwell", year: 1949 },
+  {title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
+  {title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
+  {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  {title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
+]
+
+# Initialize a hash to store author-centric data
+author_centric = {}
+
+# Initialize variables for the loop
+i = 0
+while i < books.length
+  book = books[i]
+  author = book[:author]
+  
+  # Create a new entry for the author if it doesn't exist
+  author_centric[author] ||= []
+
+  # Add book information to the author's entry
+  author_centric[author] << {title: book[:title], year: book[:year]}
+
+  # Move to the next book
+  i += 1
+end
+
+# Print the author-centric data
+puts author_centric
+
+
         # Problem -----------------------------------------------------------------
 
 Data Transformation: Complete the Data II
