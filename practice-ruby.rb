c077a15db6be950c33ade6d89d444851e2a657f2
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-10-22 09:16:25 -0700

    ETL#3 Ruby

--- practice-ruby.rb
+++ practice-ruby.rb
@@ -1,14 +1,102 @@
 
-        # Problem -----------------------------------------------------------------
+   
+# Problem -----------------------------------------------------------------
 
 
 
 # Solution =============================================
 
     
+     
+        
+        
+# Problem -----------------------------------------------------------------
+
+#Data Transformation: ETL #3
+
+Given an array of Youtube videos, for example:
+
+[
+{title: 'How to Make Wood', author_id: 4, views: 6},
+{title: 'How to Seem Perfect', author_id: 4, views: 111},
+{title: 'Review of the New "Unbreakable Mug"', author_id: 2, views: 202},
+{title: 'Why Pigs Stink', author_id: 1, views: 12}
+]
+
+and an array of authors, for example:
+
+[
+{id: 1, first_name: 'Jazz', last_name: 'Callahan'},
+{id: 2, first_name: 'Ichabod', last_name: 'Loadbearer'},
+{id: 3, first_name: 'Saron', last_name: 'Kim'},
+{id: 4, first_name: 'Teena', last_name: 'Burgess'},
+]
+
+Return a new array of videos in the following format, and only include videos that have at least 100 views:
+
+[
+{title: 'How to Seem Perfect', views: 111, author_name: 'Teena Burgess' }
+{title: 'Review of the New "Unbreakable Mug"', views: 202, author_name: 'Ichabod Loadbearer' },
+]
 
 
-        # Problem -----------------------------------------------------------------
+
+# Solution =============================================
+
+# Given data
+videos = [
+  { title: 'How to Make Wood', author_id: 4, views: 6 },
+  { title: 'How to Seem Perfect', author_id: 4, views: 111 },
+  { title: 'Review of the New "Unbreakable Mug"', author_id: 2, views: 202 },
+  { title: 'Why Pigs Stink', author_id: 1, views: 12 }
+]
+
+authors = [
+  { id: 1, first_name: 'Jazz', last_name: 'Callahan' },
+  { id: 2, first_name: 'Ichabod', last_name: 'Loadbearer' },
+  { id: 3, first_name: 'Saron', last_name: 'Kim' },
+  { id: 4, first_name: 'Teena', last_name: 'Burgess' }
+]
+
+# Initialize variables
+i = 0
+result = []
+
+# Loop through the videos array
+while i < videos.length
+  video = videos[i]
+
+  # Check if the video has at least 100 views
+  if video[:views] >= 100
+
+    # Find the author associated with the video
+    author = authors.find { |author| author[:id] == video[:author_id] }
+
+    # Create the author's full name
+    author_name = "#{author[:first_name]} #{author[:last_name]}"
+
+    # Create a formatted video hash
+    formatted_video = {
+      title: video[:title],
+      views: video[:views],
+      author_name: author_name
+    }
+
+    # Add the formatted video to the result array
+    result << formatted_video
+  end
+
+  # Move to the next video in the loop
+  i += 1
+end
+
+# Print the result
+puts "Filtered and formatted videos:"
+puts result
+    
+
+
+# Problem -----------------------------------------------------------------
 
         Data Transformation: Book Organizer
 
