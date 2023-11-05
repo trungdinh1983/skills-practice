e9b275d5128cba4e131fd7ca65fed260a73931b1
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-11-05 01:44:35 -0800

    Missing Letter

--- practice-javascript.js
+++ practice-javascript.js
@@ -5,6 +5,20 @@
 // # Solution =============================================
 
 
+// # Problem -----------------------------------------------------------------
+
+From Brute to Linear: Missing Letter
+
+A given string contains all the letters from the alphabet except for one. Return the missing letter.
+
+NOTE: You must accomplish this in O(n) time. This is also known as linear time.
+
+Input: “The quick brown box jumps over a lazy dog”
+Output: “f”
+
+// # Solution =============================================
+
+
 
 // # Problem -----------------------------------------------------------------
 
--- practice-ruby.rb
+++ practice-ruby.rb
@@ -1,7 +1,24 @@
-       
+         
 # Problem -----------------------------------------------------------------
 
 
+
+# Solution =============================================
+
+    
+
+  
+
+# Problem -----------------------------------------------------------------
+
+From Brute to Linear: Missing Letter
+
+A given string contains all the letters from the alphabet except for one. Return the missing letter.
+
+NOTE: You must accomplish this in O(n) time. This is also known as linear time.
+
+Input: “The quick brown box jumps over a lazy dog”
+Output: “f”
 
 # Solution =============================================
 
